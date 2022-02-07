40f51fc24ffccbbe60148394780e57ca1a8b4aae
Rakesh Paul <developer.rakeshpaul@gmail.com>
2022-02-07 10:12:15 +0530

    next with tailwind init

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,13 @@
+const { join } = require('path');
+// available since Nx v 12.5
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
+
+module.exports = {
+  presets: [require('../../tailwind-preset.js')],
+  content: [
+    join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
+    join(__dirname, 'components/**/*.{js,ts,jsx,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  plugins: [require('@tailwindcss/typography')],
+};
